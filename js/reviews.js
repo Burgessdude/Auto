03b360d9d265fd6d88f18d25059cfe61e564f34d
Burgessdude <143647882+Burgessdude@users.noreply.github.com>
2024-09-25 22:30:09 -0600

    Add files via upload

---
+++ js/reviews.js
@@ -0,0 +1,27 @@
+export const reviews = [
+    {
+        "name": "Luke Skywalker",
+        "stars": 4,
+        "text": "Greg's Garage was a great resource for helping us buy our brand new car. It was really expensive but we love it!"
+    },
+    {
+        "name": "Anakin Skywalker",
+        "stars": 3,
+        "text": "Greg's Garage will bring freedom, justice, and security to your new vehicle."
+    },
+    {
+        "name": "Han Solo",
+        "stars": 5,
+        "text": "Greg's Garage helped me make the Kessel Run in less than 12 parsecs."
+    },
+    {
+        "name": "Ben Kenobi",
+        "stars": 3,
+        "text": "Hello There! Greg's Garage will help you reach places that are not to be travelled lightly."
+    },
+    {
+        "name": "Darth Vader",
+        "stars": 1,
+        "text": "Greg's Garage is a spy and a member of the rebel alliance."
+    }
+]
---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+});
+
+import {reviews} from '../js/reviews.js';
+console.log(reviews)
+
+const myTarget = document.querySelector("#cards");
+
+for (let x = 0; x < reviews.length; x++){
+    const mySection = document.createElement('section')
+
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text
+
+    const myRating = document.createElement('div')
+    
+
+    for (let step = 0; step < reviews[x].stars; step++) {
+        const genIndicator = document.createElement('img')
+        genIndicator.src = "./images/star.png"
+        myRating.appendChild(genIndicator)
+    }
+
+    mySection.appendChild(myName);
+    mySection.appendChild(myRating);
+    mySection.appendChild(myReview);
+    myTarget.appendChild(mySection);
+}
